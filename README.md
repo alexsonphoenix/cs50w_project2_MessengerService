f213a9c5fa8b226e53f03e0df9fa3aeec9a7dd0d
alexsonphoenix <43697906+alexsonphoenix@users.noreply.github.com>
2019-12-09 17:38:52 -0800

    upload
    1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# cs50w_project2
-Project2
+# Project 2
+
+Web Programming with Python and JavaScript
---
+++ application.py
@@ -0,0 +1,12 @@
+import os
+
+from flask import Flask, redirect, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
